23f127e05eacd18326824a37176a738f33f85af6
Khushal Mali <khushalmali8448@gmail.com>
2025-12-22 23:44:28 +0530

    fix

--- hooks/use-convex-query.js
+++ hooks/use-convex-query.js
@@ -38,7 +38,7 @@ export const useConvexMutation = (mutaion) => {
   const mutationFn = useMutation(mutaion);
 
   const [data, setData] = useState(undefined);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
   const mutate = async (...args) => {
