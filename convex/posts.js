8044ee05b7e4b1a48e3ac94da68ba2e1e626977e
Khushal Mali <khushalmali8448@gmail.com>
2025-12-23 12:27:16 +0530

    api to update post

--- convex/posts.js
+++ convex/posts.js
@@ -98,3 +98,58 @@ export const create = mutation({
     return postId;
   },
 });
+
+// update the existing post
+export const update = mutation({
+  args: {
+    id: v.id("posts"),
+    title: v.optional(v.string()),
+    content: v.optional(v.string()),
+    status: v.optional(v.union(v.literal("draft"), v.literal("published"))),
+    tags: v.optional(v.array(v.string())),
+    category: v.optional(v.string()),
+    featuredImage: v.optional(v.string()),
+    scheduledFor: v.optional(v.number()),
+  },
+  handler: async (ctx, args) => {
+    const user = await ctx.runQuery(internal.users.getCurrentuser);
+
+    // Get the post
+    const post = await ctx.db.get(args.id);
+    if (!post) {
+      throw new Error("Post not found!");
+    }
+
+    // Check if user owns the post
+    if (post?.authorId !== user._id) {
+      throw new Error("Not authorized to update this post.");
+    }
+
+    const now = Date.now();
+    const updateData = {
+      updatedAt: now,
+    };
+
+    // Add provided fields to update
+    if (args.title !== undefined) updateData.title = args.title;
+    if (args.tags !== undefined) updateData.tags = args.tags;
+    if (args.content !== undefined) updateData.content = args.content;
+    if (args.category !== undefined) updateData.category = args.category;
+    if (args.featuredImage !== undefined)
+      updateData.featuredImage = args.featuredImage;
+    if (args.scheduledFor !== undefined)
+      updateData.scheduledFor = args.scheduledFor;
+
+    // Handle status change
+    if (args.status !== undefined) {
+      updateData.status = args.status;
+
+      if (args.status === "published" && post.status === "draft") {
+        updateData.publishedAt = now;
+      }
+    }
+
+    await ctx.db.patch(args.id, updateData);
+    return args.id;
+  },
+});
